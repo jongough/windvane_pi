f19aac9a603c359439234ea65ea84b39f46e150f
jongough <jon.gough@eclipsesystems.com.au>
2020-04-26 14:23:49 +1000

    Upadate all wxIsNaN to std::isnan [skip appveyor] [skip travis]

--- src/wind.cpp
+++ src/wind.cpp
@@ -252,7 +252,7 @@ void WindvaneInstrument_AppSmoothedWindAngle::DrawData(wxGCDC* dc, double value,
 	wxSize size = GetClientSize();
 
 	wxString text;
-	if (!wxIsNaN(value))
+    if (!std::isnan(value))
 	{
 		if (unit == _T("\u00B0"))
 			text = wxString::Format(format, value) + DEGREE_SIGN;
